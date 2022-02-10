9ada167a2e5c20eb8bc68672e1a6c748990369f8
karma007 <abhishekpandey.kr@gmail.com>
2022-02-10 23:28:47 +0530

    api created for fetch all the listed player in auction

--- backend/main.py
+++ backend/main.py
@@ -1,54 +1,93 @@
 import json
-from flask import Flask
+from flask import Flask, jsonify
 import sqlite3
 import csv
+from flask_sqlalchemy import SQLAlchemy
+# from team_model import Player
 
-print("[INFO] Starting server...")
 app = Flask(__name__)
-print("sqlite3 version:", sqlite3.version)
-new_players = []
-
-# open a csv file to read and then convert them into json
-with open('data/players_data.csv', 'r') as f:
-    reader = csv.reader(f)
-    players_data = list(reader)
-    for player in range(1, len(players_data)):
-        new_player = {
-            players_data[0][0]: players_data[player][0],
-            players_data[0][1]: players_data[player][1],
-            players_data[0][2]: players_data[player][2],
-            players_data[0][3]: players_data[player][3],
-            players_data[0][4]: players_data[player][4],
-            players_data[0][5]: players_data[player][5],
-            players_data[0][6]: players_data[player][6],
-            players_data[0][7]: players_data[player][7],
-            players_data[0][8]: players_data[player][8],
-            players_data[0][11]: players_data[player][12],
-            players_data[0][12]: players_data[player][13],
-            players_data[0][13]: players_data[player][14],
-            players_data[0][14]: players_data[player][15],
-            players_data[0][15]: players_data[player][16],
-            players_data[0][16]: players_data[player][17],
-            players_data[0][17]: players_data[player][18],
-            players_data[0][18]: players_data[player][19],
-            players_data[0][19]: players_data[player][len(players_data[player]) - 1]
-        }
-        new_players.append(new_player)
-        with open('data/players_data.json', 'w') as f:
-            json.dump(new_players, f)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///players.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def main():
-    return 'Hello World!'
+class Player(db.Model):
+    list_sr_no = db.Column(db.String, primary_key=True)
+    set_no = db.Column(db.String, nullable=False)
+    set_2022 = db.Column(db.String, nullable=False)
+    first_Name = db.Column(db.String, nullable=False)
+    surname = db.Column(db.String, nullable=False)
+    country = db.Column(db.String, nullable=False)
+    state_association = db.Column(db.String, nullable=True)
+    dob = db.Column(db.String, nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    test_caps = db.Column(db.Integer, nullable=True)
+    odi_caps= db.Column(db.Integer, nullable=True)
+    t20_caps = db.Column(db.Integer, nullable=True)
+    ipl = db.Column(db.Integer, nullable=True)
+    previous_IPLTeams = db.Column(db.String, nullable=True)
+    team_2021 = db.Column(db.String, nullable=True)
+    ipl_played_in_2021 = db.Column(db.Integer, nullable=True)
+    C_U_A = db.Column(db.String, nullable=False)
+    base_salary_in_lakhs = db.Column(db.Integer, nullable=False)
 
-@app.route("/players")
-def get_all_players():
-    players = []
-    # open the json file and read it and return response as json
-    with open('data/players_data.json', 'r') as f:
-        players = json.load(f)
-    return json.dumps(players)
+db.create_all()
+
+
+class Player_data:
+
+    def store_player_to_db():        
+        # open a csv file to read and then convert them into json
+        with open('data/players_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            players_data = list(reader)
+            for player in range(1, len(players_data)):
+                new_player = {
+                    "list_sr_no": players_data[player][0],
+                    "set_no": players_data[player][1],
+                    "set_2022": players_data[player][2],
+                    "first_Name": players_data[player][3],
+                    "surname": players_data[player][4],
+                    "country": players_data[player][5],
+                    "state_association": players_data[player][6],
+                    "dob": players_data[player][7],
+                    "age": players_data[player][8],
+                    "test_caps": players_data[player][12],
+                    "odi_caps": players_data[player][13],
+                    "t20_caps": players_data[player][14],
+                    "ipl": players_data[player][15],
+                    "previous_IPLTeams": players_data[player][16],
+                    "team_2021": players_data[player][17],
+                    "ipl_played_in_2021": players_data[player][18],
+                    "C_U_A": players_data[player][19],
+                    "base_salary_in_lakhs": players_data[player][len(players_data[player]) - 1]
+                }
+                # save to database
+                player_to_save_in_db = Player(**new_player)
+                db.session.add(player_to_save_in_db)
+                db.session.commit()
+    
+
+    @app.route("/players")
+    def get_all_players():
+        players = Player.query.all()
+        all_players = []
+        for player in players:
+            all_players.append({
+                "first_Name": player.first_Name,
+                "surname": player.surname,
+                "country": player.country,
+                "age": player.age,
+                "test_caps": player.test_caps,
+                "odi_caps": player.odi_caps,
+                "t20_caps": player.t20_caps,
+                "ipl": player.ipl,
+                "ipl_played_in_2021": player.ipl_played_in_2021,
+                "C_U_A": player.C_U_A,
+                "base_salary_in_lakhs": player.base_salary_in_lakhs
+            })
+        return jsonify(players = all_players)
 
 if __name__ == '__main__':
+    main_class = Player_data()
     app.run(debug=True)
